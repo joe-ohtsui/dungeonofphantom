906302474c359f16d11211d56d5b4a63479b0cbd
joe-ohtsui <jo0035jp@yahoo.co.jp>
2017-06-01 01:57:20 +0900

    フリックで移動する

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,60 @@ public class PlayerController : MonoBehaviour
     {
         player = GetComponent<Actor>();
     }
+
+	private Vector3 touchStartPos;
+	private Vector3 touchEndPos;
+
+	void Flick()
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0))
+		{
+			touchStartPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+		}
+		if (Input.GetKeyUp (KeyCode.Mouse0))
+		{
+			touchEndPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+			GetDirection ();
+		}
+	}
+
+	void GetDirection()
+	{
+		float dx = touchEndPos.x - touchStartPos.x;
+		float dy = touchEndPos.y - touchStartPos.y;
+
+		if (Mathf.Abs (dy) < Mathf.Abs (dx))
+		{
+			if (30 < dx)
+			{
+				//右向きにフリックされた
+				player.setDest (player.pos.toLeft ());
+			}
+			else if (-30 > dx)
+			{
+				//左向きにフリックされた
+				player.setDest (player.pos.toRight ());	
+			}
+		}
+		else if (Mathf.Abs (dx) < Mathf.Abs (dy))
+		{
+			if (30 < dy)
+			{
+				//上向きにフリックされた
+				player.setDest (player.pos.move ((player.pos.direction + 2) % 4));
+			}
+			else if (-30 > dy)
+			{
+				//下向きにフリックされた
+				player.setDest (player.pos.move (player.pos.direction));
+			}
+		}
+		else
+		{
+			//タップされた
+			player.setDest (player.pos.move (player.pos.direction));
+		}
+	}
 	
 	void Update ()
     {
@@ -34,6 +88,7 @@ public class PlayerController : MonoBehaviour
             {
                 player.actphase = Actor.Phase.ATTACK_START;
             }
+			Flick ();
         }
         transform.LookAt(player.getLookAt());
     }
