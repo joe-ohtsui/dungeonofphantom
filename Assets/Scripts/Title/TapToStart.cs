236a1264542ad58093473e85daec2a6686d071e9
joe-ohtsui <jo0035jp@yahoo.co.jp>
2017-06-23 00:53:11 +0900

    タイトル画面追加
    タイトル画面追加

---
+++ Assets/Scripts/Title/TapToStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapToStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetTouch(0).touch.phase == TouchPhase.Ended)
+		//{
+			// タッチ終了
+
+		//}
+	}
+}
